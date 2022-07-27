f0160232fa0071299b2bf4a7bc2f2e451f9ca773
harshitjain25 <109205787+harshitjain25@users.noreply.github.com>
2022-07-27 23:26:15 +0530

    Add files via upload

---
+++ 27th July 2022/additionKinArray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, k, i;
+    cout<<"Enter no. of elements: ";
+    cin >> n;
+    int a[n + 1];
+    cout << "enter elements of a \n";
+    for (int i = 0; i < n; i++)
+    {
+
+        cin >> a[i];
+    }
+    cout << "enter the no. t be added";
+    cin >> k;
+    for (i = n - 1; i >= 0; i--)
+    {
+        if (a[i] > k)
+        {
+            a[i + 1] = a[i];
+        }
+        else
+            break;
+    }
+    a[i + 1] = k;
+}
