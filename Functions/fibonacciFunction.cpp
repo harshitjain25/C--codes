a91d676424cf539c61f1dfb938c2e2ad7f7a5eda
Harshit Jain <109205787+harshitjain25@users.noreply.github.com>
2022-08-16 16:30:34 +0530

    Add files via upload

--- Functions/fibonacciFunction.cpp
+++ Functions/fibonacciFunction.cpp
@@ -11,20 +11,21 @@ int main()
 }
 void fibonacci(int n)
 {
-    int a = 0, b = 1,c;
-    for (int i = 1; i <= n-2; i++)
+    int a = 0, b = 1, c;
+    if (n == 1)
     {
-        if (i == 1)
+        cout << " " << a;
+    }
+    else if (n >= 2)
+    {
+        cout << " " << a << " " << b;
+
+        for (int i = 1; i <= n - 2; i++)
         {
-            cout << " " << a;
+            c = a + b;
+            a = b;
+            b = c;
+            cout << " " << c;
         }
-        if (i == 2)
-        {
-            cout << " " << b;
-        }
-        c = a + b;
-        a = b;
-        b = c;
-        cout << " " << c;
     }
 }
