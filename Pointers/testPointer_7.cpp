0be433d29a0c244eb2fc2b97c03abacd7d74c358
Harshit Jain <109205787+harshitjain25@users.noreply.github.com>
2022-09-01 21:20:42 +0530

    Add files via upload

---
+++ Pointers/testPointer_7.cpp
@@ -0,0 +1,24 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     const int a=10;
+//     const int *p=&a;
+//     p++;
+//     cout<<*p;
+//     return 0;
+// }
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+  //  int * const p = &a; //p is a constant pointer,it will point to a only.
+    //(*p)++;
+
+    //cout << *p;
+    //p++;
+    float *k;
+    k=&a;
+    return 0;
+}
