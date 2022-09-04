4aa45bd3be7917b67a8e0d953f4310f4f2decc6f
Harshit Jain <109205787+harshitjain25@users.noreply.github.com>
2022-09-04 22:35:34 +0530

    Add files via upload

---
+++ Object Oriented Programming/vartika.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+class Person
+{
+    char name[20];
+    int age;
+
+public:
+    int r;
+    void getdata(void);
+    void putdata(void);
+};
+void Person ::getdata(void)
+{
+    cout << "Enter Details: ";
+    cin >> name;
+    cin >> age;
+    cin >> r;
+}
+void Person ::putdata(void)
+{
+    cout << name;
+    cout << age;
+    cout << r;
+}
+int main()
+{
+    Person a1, a2;
+    a1.getdata();
+    a1.putdata();
+    a2.getdata();
+    a2.putdata();
+   
+   
+    return 0;
+}
