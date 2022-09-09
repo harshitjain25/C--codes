232a92874c5c5bb3fbd95dc966b4738a2289990a
Harshit Jain <109205787+harshitjain25@users.noreply.github.com>
2022-09-09 23:39:26 +0530

    Add files via upload

---
+++ Pointers/arrayOfStrings.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=5;
+    char s[10][20];
+    for(int i=0;i<n;i++)
+    {
+        cin.getline(s[i],20,'\n');
+    }
+    cout<<s;
+    return 0;
+}
